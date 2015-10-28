43fbdffaf12c98bebbc37043fbf11e1dcb6160d8
ahrobinson <ahrobins3@gmail.com>
2015-10-28 18:13:32 -0500

    responds with index.html contents on get request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,32 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs');
+
+
+var defaultCorsHeaders = {
+ "access-control-allow-origin": "*",
+ "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+ "access-control-allow-head ers": "content-type, accept",
+ "access-control-max-age": 10,
+ "Content-Type": "text/html"
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var statusCode = 200;
+  res.writeHead(statusCode, defaultCorsHeaders);
+  if( req.method === "GET" && req.url === '/') {
+    fs.readFile(archive.paths.siteAssets + '/index.html', function (err, data) {
+      if (err) throw err;
+      var chunk = '';
+      chunk = chunk + data;
+
+      // console.log("chunk");
+       res.end(chunk);
+    });
+  }
+  if (req.method === "GET" && req.url === "/" + )
+
+
+
 };
